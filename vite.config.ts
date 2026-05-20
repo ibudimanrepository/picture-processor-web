7742fe37107f37eba39b7dadb3554b7f21f4e922
Imansyah Budiman <ibudiman@Imansyahs-MacBook-Air.local>
2026-05-20 08:18:45 +0800

    Fix base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/picture-processor-web/',
   plugins: [react()],
   build: {
     outDir: 'docs',
